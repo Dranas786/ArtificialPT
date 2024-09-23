23368a0a83805ef45cdadca550f7cb5d2a9b95bf
Dranas786 <dranas2002@gmail.com>
2024-09-22 19:57:47 -0600

    message_here

--- artificial-pt/src/App.js
+++ artificial-pt/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Rate your exercise</h1>
+      <h2>0.0</h2>
+      <form>
+        <div className="basic">
+          <input type="text" placeholder={"Bench Press"} />
+          <input type="text" placeholder={"8 (Number of reps)"} />
+          <input type="text" placeholder={"2 (Number of sets)"} />
+        </div>
+
+        <button type="submit">Add another exercise</button>
+      </form>
+      <div className="exercises">
+        <div className="exercise">
+          <div className="left">
+            <div className="name"> Bench press</div>
+            <div className="muscle">Chest</div>
+          </div>
+          <div className="right">
+            <div className="Score"> 9.0</div>
+            <div className="rep_set">8, 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
